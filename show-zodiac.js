e443622a949dc9fc0689356014f984c199e43ae4
kosmos02 <alexandrgabe@gmail.com>
2020-12-03 13:23:40 -0700

    finished MVP. put all signs on show page and attributes.

--- show-zodiac.js
+++ show-zodiac.js
@@ -1,8 +1,8 @@
 const searchParams = new URLSearchParams(window.location.search)
 const name = searchParams.get('name')
 
-const $main = document.querySelector('main')
-const $p = document.querySelector('.zodiac-name')
+const $zodiacCard = document.querySelector('#zodiac-card')
+
 
 
 
@@ -15,14 +15,69 @@ fetch(`https://sameer-kumar-aztro-v1.p.rapidapi.com/?day=today&sign=${name}`, {
 })
 .then(response => response.json())
 .then(sign => {
-
-    const $signColor = document.createElement('h1')
-    $signColor.textContent = `Color: ${sign.color}` 
-	$main.append($signColor)
-
-	
+	console.log(sign)
+	displayName(sign);
+	displayDateInfo(sign);
+	displayMood(sign);
+	displayHoroscope(sign);
+	displayColor(sign);
+	displayCompatability(sign);
+	displayLuckyTime(sign);
+	displayLuckyNumber(sign)
 	
 })
 .catch(err => {
 	console.error(err);
 });
+
+function displayName(sign){
+	const $signName = document.createElement('h1')
+    $signName.textContent = name 
+	$zodiacCard.append($signName)
+}
+
+function displayDateInfo(sign){
+	const $signDateRange = document.createElement('p')
+	$signDateRange.textContent =  sign.date_range
+	$zodiacCard.append($signDateRange)
+
+	const $signCurrentDate = document.createElement('p')
+	$signCurrentDate.textContent = `Today's Date: ${sign.current_date}`
+	$zodiacCard.append($signCurrentDate)
+}
+
+function displayMood(sign) {
+	const $signMood = document.createElement('p')
+	$signMood.textContent = `Today's Mood: ${sign.mood}`
+	$zodiacCard.append($signMood)
+}
+
+function displayHoroscope(sign) {
+	const $signHoroscope = document.createElement('p')
+	$signHoroscope.textContent = `Today's Horoscope: ${sign.description}`
+	$zodiacCard.append($signHoroscope)
+}
+
+function displayColor(sign){
+	const $signColor = document.createElement('p')
+	$signColor.textContent = `Color: ${sign.color}`
+	$zodiacCard.append($signColor)
+}
+
+function displayCompatability(sign){
+	const $signCompatibility = document.createElement('p')
+	$signCompatibility.textContent = `Compatibility: ${sign.compatibility}`
+	$zodiacCard.append($signCompatibility)
+}
+
+function displayLuckyTime(sign){
+	const $signLuckyTime = document.createElement('p')
+	$signLuckyTime.textContent = `Today's Lucky Time: ${sign.lucky_time}`
+	$zodiacCard.append($signLuckyTime)
+}
+
+function displayLuckyNumber(sign){
+	const $signLuckyNumber = document.createElement('p')
+	$signLuckyNumber.textContent = `Today's Lucky Number: ${sign.lucky_number}`
+	$zodiacCard.append($signLuckyNumber)
+}
